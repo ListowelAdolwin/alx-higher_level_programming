3665ed1005548818883335872c468229efa8db3e
ListowelAdolwin <listoweladolwin@gmail.com>
2022-06-28 16:22:54 +0000

    Determine the sign of the last digit of a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num = number % 10
-print("The last digit of {} is {} ".format(number, num), end="")
-if (num > 5):
-    print("and is greater than 5")
-elif (num == 0):
-    print("and is 0")
-elif (num < 6) and (num != 0):
-    print("and is less than 6 and not 0")
+if number < 0:
+    number = number * -1
+    num = number % 10
+    print("Last digit of {} is {} ".format(number * -1, num * -1), end="")
+    if num > 5:
+        print("and is greater than 5")
+    elif num == 0:
+        print("and is 0")
+    elif (num < 6) and (num != 0):
+        print("and is less than 6 and not 0")
+else:        
+    num = number % 10
+    print("Last digit of {} is {} ".format(number, num), end="")
+    if (num > 5)    :
+        print("and is greater than 5")
+    elif (num == 0):
+        print("and is 0")
+    elif (num < 6) and (num != 0):
+        print("and is less than 6 and not 0")
