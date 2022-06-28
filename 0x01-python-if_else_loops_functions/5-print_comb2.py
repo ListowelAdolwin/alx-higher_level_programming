8dc44730abf5c7b90ac0d03a9531447a8a423429
ListowelAdolwin <listoweladolwin@gmail.com>
2022-06-28 17:39:11 +0000

    Print two digit numbers in special formats

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
+    if number < 99:
         print("{:0>2d}, ".format(number), end="")
-    elif number < 99:
-        print("{}, ".format(number), end="")
     else:
         print(number)
