1316cbfc6d79edae02fabeef54ad40a960ca5627
ListowelAdolwin <listoweladolwin@gmail.com>
2022-06-28 19:08:37 +0000

    Print the alphabet in alternating reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-
-for letter in reversed(range(97, 123)):
-    print("{:c}".format(letter if (letter % 2 == 0) else (letter - 32)), end='')
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
