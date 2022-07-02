3d435c41f9c9ff0fadab544bf9e7239cb96398ba
ListowelAdolwin <listoweladolwin>
2022-07-02 11:01:45 +0000

    Print a list in reversed order.Corrected an indentation error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,4 +6,4 @@ def print_reversed_list_integer(my_list=[]):
         pass
     else:
         for i in range(-1, -(len(my_list) + 1), -1):
-        print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
