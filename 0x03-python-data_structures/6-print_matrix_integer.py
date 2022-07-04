cb08de97c7d354dd67d1a8033da19c501c7e7f8e
ListowelAdolwin <listoweladolwin>
2022-07-04 13:45:12 +0000

    Corrected. Added a condition to print the matrix and $ with no spacing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,9 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d} ".format(matrix[i][j]), end="")
-        print()
+   for i in matrix:
+       for j in i:
+           if j != 0:
+               print(" ", end='')
+           print("{:d}".format(j), end='')
+       print()
