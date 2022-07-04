739bd20202d750a5aec3c8b0421a6c977a0dfe4e
ListowelAdolwin <listoweladolwin>
2022-07-04 14:50:53 +0000

    Find the maximum value of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        max = my_list[0]
+        for value in my_list:
+            if value > max:
+                max = value
+        return max
