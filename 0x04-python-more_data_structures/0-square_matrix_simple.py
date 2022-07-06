ab9225696eb858e2b74763184a820badd5703f2f
ListowelAdolwin <listoweladolwin>
2022-07-06 23:42:46 +0000

    Print the squares of the members of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for i in matrix:
-        for j in i:
-            new_matrix.append(j ** 2)
-    return new_matrix
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
