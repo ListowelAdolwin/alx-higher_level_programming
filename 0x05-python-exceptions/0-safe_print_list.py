c306cd7a961be2ebe282ccff54fd716eb6b9aac4
ListowelAdolwin <listoweladolwin>
2022-07-18 22:01:24 +0000

    Print a specified number of elements of a list and make use of try and catch

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    count = 0
+    try:
+        for item in my_list:
+        while(i <= x):
+            print(my_list[i], end='')
+            count += 1
+        print()
+
+    except Exception:
+        pass
+    return (count)
