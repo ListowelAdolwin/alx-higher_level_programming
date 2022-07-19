aa3d9e98e48c15f9e29d78b40c8541851af0c975
ListowelAdolwin <listoweladolwin>
2022-07-19 00:11:41 +0000

    Print and count an integer list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 
-def safe_print_list_integers(my_list=[], x=0):
-    c = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            c += 1
-        except (ValueError, TypeError):
-            continue
-    print()
-    return 
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
+        return False
+    else:
+        return True
