7b2b46bf8672a05dfc2b544965d0282690d46b26
ListowelAdolwin <listoweladolwin>
2022-07-19 00:03:34 +0000

    Print elements of a list and include exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,14 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    count = 0
-    try:
-        for item in my_list:
-            while(i <= x):
-                print(my_list[i], end='')
-                count += 1
-
-    except Exception:
-        pass
-        print()
-    return (count)
+    for index in range(x):
+        try:
+            print(my_list[index], end="")
+            i += 1
+        except IndexError:
+            break
+    print()
+    return i
