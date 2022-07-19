6dca2be85ad2236df1f5a673189e09fdfb0970c9
ListowelAdolwin <listoweladolwin>
2022-07-19 00:09:50 +0000

    Safe printing of an integer list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-def safe_print_integer(value):
-    state = True
-    try:
-        print("{:d}".format(value))
-
-    except TypeError:
-        state = False
-        print("Opps! Wrong data type")
-
-    return (state)
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return 
