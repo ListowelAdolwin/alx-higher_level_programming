f652c42db6a5f48fd5942ca998f540b2e0663e6c
ListowelAdolwin <listoweladolwin>
2022-07-20 13:29:51 +0000

    Access update and private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+A class that defines a square
+
+"""
+
+
+class Square:
+    """
+    This class defines a square on some conditions:
+        . with a private instance variable
+        . instantiating it with an optional size attribute
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the class
+
+        """
+
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def size(self):
+        """
+        A setter to return size
+
+        """
+
+        return self.__size
+
+    def size(self, value):
+        """
+        A property setter to set the value to size
+
+        """
+
+        self.__size = value
+
+
+    def area(self):
+        """
+        Returns the area of the square
+
+        """
+
+        return(self.__size ** 2)
