96565f45ed647a0002adfc1af5a9144b664f8a61
ListowelAdolwin <listoweladolwin>
2022-07-20 12:07:05 +0000

    A square class that describes a square with an instance variable, size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""
+A simple program with only a class that describes
+a square.
+In the square, is a private instance variable, size
+
+"""
+
+
+class Square:
+    """
+    A class describing a square.
+    With an instance variable, size
+
+    """
+    def __init__(self, size):
+        self.size = size
