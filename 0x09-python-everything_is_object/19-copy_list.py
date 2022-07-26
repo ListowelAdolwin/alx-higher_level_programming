da2e5cf769d0b887055546d7e27d295fbb71bd45
ListowelAdolwin <listoweladolwin>
2022-07-26 22:32:00 +0000

    Copy a list object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-"""
-A program with one function
-that returns the copy of a list
-
-"""
-
-
 def copy_list(l):
-    """
-    Return the copy of a list
-
-    """
-
-    l_copy = l[:]
-    return l_copy
+    return l[:]
