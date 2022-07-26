5e14043e0703c2cfb6798bf056a19f7b2595aef2
ListowelAdolwin <listoweladolwin>
2022-07-26 00:27:34 +0000

    Compare rectangles. Corrected

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -113,6 +113,6 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
-            return rect_2
+            return rect_1 
         if rect_1.area() < rect_2.area():
             return rect_2
