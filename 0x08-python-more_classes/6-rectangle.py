0ce63ca36b76c7c0cfe8d351d58153c5e3b705ed
ListowelAdolwin <listoweladolwin>
2022-07-26 00:21:50 +0000

    Corrected an indetation error

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
 
     number_of_instances = 0
 
-def __init__(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
         Args:
             width: width of the rectangle
