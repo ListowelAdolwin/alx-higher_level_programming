4af17da3270134ebfea76dba149a9699982273d8
ListowelAdolwin <listoweladolwin>
2022-07-26 23:09:20 +0000

    Create a locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+"""
+Create a locked class
+
+"""
 
 
 class LockedClass:
+    """
+    A class that prevents the instanciation
+    of attributes except for first_name
+
+    """
+
     __slots__ = ['first_name']
 
     def __init__(self, first_name=''):
+        """
+        Initializes first_name
+        
+        """
+
         self.first_name = first_name
