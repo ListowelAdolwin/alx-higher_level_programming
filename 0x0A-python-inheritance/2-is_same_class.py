67692fa2c2981d53c8d2019e8306e54eee30a46d
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-01 23:12:06 +0000

    Isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -12,7 +12,7 @@ def is_same_class(obj, a_class):
     instance of another
 
     """
-    if (isinstance(obj, a_class)):
+    if (type(obj) is a_class):
         return True
     esle:
         return False
