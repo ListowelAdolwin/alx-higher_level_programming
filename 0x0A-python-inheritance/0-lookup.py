2a1d6a2b50e38b9d4e78b98d6c18e1e0236c3b7c
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-01 22:38:05 +0000

    Return the attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A program with just one function
+
+"""
+
+def lookup(obj):
+    """
+    A function that returns the available attributes
+    and methods of an object
+
+    """
+    my_list = dir(obj)
+    return my_list
