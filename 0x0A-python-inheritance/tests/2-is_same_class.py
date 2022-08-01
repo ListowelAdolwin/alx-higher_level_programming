e6a2c490a8fd0a61755870e9450d0dacea39064b
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-01 23:01:26 +0000

    Determine is an object is an instance of another

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A program to tell whether an object is
+or not an instance of another
+
+"""
+
+
+def is_same_class(obj, a_class):
+    if (isinstance(obje, a_class)):
+        return True
+    esle:
+        return False
