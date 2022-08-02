be4518af714a5aba0d072f937a8127450ba48dc8
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-02 21:21:25 +0000

    Read and print files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
     Read from a file and print it
 
     """
-    with open("filename", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         text = f.read()
         print(text, end="")
