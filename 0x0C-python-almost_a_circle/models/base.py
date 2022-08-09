e0c2f8a1b9e8ac2fbce57a1c5d09c6968853e7e4
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-09 00:56:26 +0000

    Base class for other classes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,7 +63,7 @@ class Base:
             jstr = cls.to_json_string([o.to_dictionary() for o in list_objs])
         filename = cls.__name__ + ".json"
         with open(filename, 'w') as f:
-                f.write(jstr)
+            f.write(jstr)
 
     @staticmethod
     def from_json_string(json_string):
@@ -72,12 +72,12 @@ class Base:
             - json_string: string to convert to list
         """
 
-        l = []
+        lst = []
         if json_string is not None and json_string != '':
             if type(json_string) != str:
                 raise TypeError("json_string must be a string")
-            l = json.loads(json_string)
-        return l
+            lst = json.loads(json_string)
+        return lst
 
     @classmethod
     def create(cls, **dictionary):
@@ -98,15 +98,15 @@ class Base:
         """Returns a list of instances."""
 
         filename = cls.__name__ + ".json"
-        l = []
+        lst = []
         list_dicts = []
         if os.path.exists(filename):
             with open(filename, 'r') as f:
                 s = f.read()
                 list_dicts = cls.from_json_string(s)
                 for d in list_dicts:
-                    l.append(cls.create(**d))
-        return l
+                    lst.append(cls.create(**d))
+        return lst
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
@@ -140,7 +140,7 @@ class Base:
         """
 
         filename = cls.__name__ + ".csv"
-        l = []
+        lst = []
         if os.path.exists(filename):
             with open(filename, 'r') as f:
                 reader = csv.reader(f, delimiter=',')
@@ -154,8 +154,8 @@ class Base:
                         for j, e in enumerate(row):
                             if e:
                                 setattr(i, fields[j], int(e))
-                        l.append(i)
-        return l
+                        lst.append(i)
+        return lst
 
     @staticmethod
     def draw(list_rectangles, list_squares):
