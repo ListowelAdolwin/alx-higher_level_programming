528746cd0106f57535ac82dec6c5d5e8a704724e
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-06 18:41:51 +0000

    Display top 3 temperatures

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+select city, value from temperatures order by value desc limit 3;
+-- Display top 4 temperatures
+-- Order in descending
+SELECT city, value
+FROM temperatures
+ORDER BY  value DESC
+LIMIT 3
