c8234e080a631e3ac7ce48d424cceadb17de68f1
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-06 19:01:07 +0000

    Average

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,4 @@
--- Display top 4 temperatures
--- Order in descending
-SELECT city, AVG(value) AS average
-FROM temperatures
-WHERE month = 7 OR month = 8
-GROUP BY city
-ORDER BY  average DESC
-LIMIT 3;
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+-- displays the top 3 values of a column in a table
+SELECT `city`, AVG(`value`) 'avg_temp' FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
+
