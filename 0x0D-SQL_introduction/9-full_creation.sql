7511c776b27a689525560f544ee57c19f3ca4d91
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-06 17:03:29 +0000

    Create and populate a table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256),
 	score INT);
 
-INSERT INTO second_table VALUES(id = 1, name = "John", score = 10);
+INSERT INTO second_table VALUES(1, "John", 10);
 INSERT INTO second_table VALUES(2, "Alex", 3);
 INSERT INTO second_table VALUES(3, "Bob", 14);
 INSERT INTO second_table VALUES(4, "George", 8);
