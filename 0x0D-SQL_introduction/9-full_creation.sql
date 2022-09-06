536f947f934a27191b4c7b83fdba52da6601dc6c
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-06 16:57:58 +0000

    Corrected quotes

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256),
 	score INT);
 
-INSERT INTO second_table VALUES(id = 1, name = “John”, score = 10);
-INSERT INTO second_table VALUES(id = 2, name = “Alex”, score = 3);
-INSERT INTO second_table VALUES(id = 3, name = “Bob”, score = 14);
-INSERT INTO second_table VALUES(id = 4, name = “George”, score = 8);
+INSERT INTO second_table VALUES(id = 1, name = "John", score = 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
