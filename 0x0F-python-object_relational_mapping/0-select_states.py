b6caac287b42e2c6ae33e7a15ade7d53b145a54d
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-06 13:54:48 +0000

    added a __name__ conditiion

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 
-import sys
-import MySQLdb
+if __name__ == '__main__':
+    import sys
 
-db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], port=3307, db=sys.argv[3])
-cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], port=3307, db=sys.argv[3])
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
-states = cur.fetchall()
+    states = cur.fetchall()
 
-for row in states:
-    print(row)
+    for row in states:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
