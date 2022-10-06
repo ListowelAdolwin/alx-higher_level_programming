434757d55acb1bd78df2190687d2db393fc86b01
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-06 22:59:01 +0000

    List all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+List all cities from the database specified
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], port=3307, db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                 ORDER BY states.id ASC;", (sys.argv[4]))
+
+    cur.execute(
+                "SELECT cities.id, cities.name, states.name FROM cities \
+                 INNER JOIN states ON cities.state_id = states.id \
+                 ORDER BY cities.id ASC;"
+        )
+
+    states = cur.fetchall()
+
+    for row in states:
+        print(row)
+
+    cur.close()
+    db.close()
