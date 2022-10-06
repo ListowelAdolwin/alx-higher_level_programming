655edd436094e731eca949438009e7133dc3c210
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-06 22:58:06 +0000

    prevent injection attack

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+List all states from the database specified
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], port=3307, db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                 ORDER BY states.id ASC;", (sys.argv[4]))
+
+    states = cur.fetchall()
+
+    for row in states:
+        print(row)
+
+    cur.close()
+    db.close()
