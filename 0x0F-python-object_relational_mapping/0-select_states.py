fab442eb477d7564aece92cd3117b963b1f4b162
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-06 15:35:40 +0000

    Corrected port number

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+List all states from the database specified
+"""
 
 
 if __name__ == '__main__':
+    import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], port=3307, db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
