e345b49472e8a0a88ed668a440c75f2ff701393b
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 10:19:45 +0000

    Added quotes to allow the query take strings

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE states.name='{}'\
+    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
                  ORDER BY states.id ASC;".format(sys.argv[4]))
 
     states = cur.fetchall()
