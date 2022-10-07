6a1ce537c1b0afff603cf399944e99e4987c32a9
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 14:34:32 +0000

    corrected mispelling of '__tablename__'

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -18,7 +18,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    __table__ = 'states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
