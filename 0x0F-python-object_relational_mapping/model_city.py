f8deb2bac420e8c2715c16f0e5435b9c3ebc42d1
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 22:01:42 +0000

    City class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Create a City class and maps it to the states table
+"""
+
+
+from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+
+class City(Base):
+    """
+    City class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship('State', back_populates='cities')
