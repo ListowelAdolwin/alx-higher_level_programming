da1530dadaf0bbe1fb93453b4df39d0424294d48
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 16:01:40 +0000

    Removed redundant imports

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,15 +4,9 @@ Create a State class and map it to the states table
 """
 
 
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Integer, String, Column
 
-"""
-engine = create_engine(
-      "mysql+pymysql://root:passwd@localhost/db?host=localhost?port=3306")
-Session = sessionmaker(bind=engine) """
 
 Base = declarative_base()
 
