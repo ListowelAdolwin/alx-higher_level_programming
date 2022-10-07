28707f8bc74a5ad2ba24b2d2b02f7a1cc6497a6b
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 10:07:58 +0000

    Corrected query and added BINARY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
                 ORDER BY states.id ASC;")
 
     states = cur.fetchall()
