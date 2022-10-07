eea1ef5c34a4e58adde8697f9b0be677c9305a74
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 22:59:47 +0000

    Fetch all objects from the City class

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     from sqlalchemy.orm import sessionmaker
     from model_state import State, Base
     from model_city import City
+    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
