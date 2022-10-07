2a8949557e29c431151788ce74713f8ed6c299df
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 10:41:39 +0000

    Corrected port number

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     import MySQLdb
 
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                         passwd=sys.argv[2], port=3307, db=sys.argv[3])
+                         passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
 
     cur.execute(
