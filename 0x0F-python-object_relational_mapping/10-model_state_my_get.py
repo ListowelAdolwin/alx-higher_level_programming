8c5dd8cef20cede050c389b5cf9ba086ff5b6b0c
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-07 21:23:10 +0000

    Print states by their name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+List all State objects from the database hbtn_0e_6_usa
+"""
+
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)()
+
+    results = Session.query(State)\
+        .filter(State.name == sys.argv[4])\
+        .order_by(State.id).first()
+    if results:
+        print("{}".format(results.id))
+
+    else:
+        print("Not found")
