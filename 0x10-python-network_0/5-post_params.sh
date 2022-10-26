fe67b05b8a60d9c4c5fac5010615f7e3aea947d0
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-26 15:05:37 +0000

    Send a POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s "$1" -X POST -d "email=test%40gmail.com" -d "subject=I will always be here for PLD"
-
+# Sends a POST request to a given URL, and displays the body of the response
+curl -sL -X POST -d 'email=test%40gmail.com&subject=I+will+always+be+here+for+PLD' "$1"
